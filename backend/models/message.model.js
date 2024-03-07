048c6d4e59843c31daaa27a04faddc2eb172ea73
Aaryan Mavani <mavaniaaryan85@gmail.com>
2024-03-07 14:39:54 +0530

    1st Part Completed -backend

---
+++ backend/models/message.model.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const existingMessageModel = mongoose.models["Message"];
+
+// If it exists, delete it
+if (existingMessageModel) {
+  delete mongoose.models["Message"];
+}
+
+const messageSchema = new mongoose.Schema(
+  {
+    senderId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    receiverId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Message = mongoose.model("Message", messageSchema);
+export default Message;
